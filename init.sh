b9df0ca243beeb949a6f5964bc1cf2ff356a1856
Oleksandr Mamonov <omamo@softserveinc.com>
2019-03-14 16:15:11 +0200

    'lol'

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 cd /home/box/web/
 sudo gunicorn -c /home/box/web/etc/gunicorn.conf -b 0.0.0.0:8080 hello:app
