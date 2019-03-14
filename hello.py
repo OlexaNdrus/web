6e339eba25a5b432663b70b8e1486787dd8f5cdd
Oleksandr Mamonov <omamo@softserveinc.com>
2019-03-14 16:28:20 +0200

    'lol'

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
 bind = '0.0.0.0:8080'
 
-def app (environ,start_response):
+
+def app(environ, start_response):
     status = '200 OK'
-    headers= [('Content-type','text/plain')]
-    start_response(status,headers)
-    body="\r\n".join(environ['QUERY_STRING'].split('&'))
-    return [body]
+    response_headers = [('Content-Type', 'text/plain')]
+    start_response(status, response_headers)
+    resp = environ['QUERY_STRING'].split("&")
+    resp = [item + "\r\n" for item in resp]
+    return resp
+
