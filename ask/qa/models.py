d5776307cb57ed509b199aca1bcf1d4fe10f1292
OlexaNdrus <abysho666@gmail.com>
2019-03-26 16:37:12 +0200

    mysql

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    title=models.CharField(default="", max_length=255)
+    text = models.TextField(default="", max_length=2055)
+    addet_at = models.DateField(blank=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, default="", on_delete=models.SET_DEFAULT)
+    likes = models.ManyToManyField(User)
+
+
+class Answer(models.Model):
+    text = models.TextField(default="", max_length=2055)
+    addet_at = models.DateField(blank=True)
+    question = models.ForeignKey(Question, default="", on_delete=models.SET_DEFAULT)
+    auther = models.ForeignKey(User,default="", on_delete=models.SET_DEFAULT)
+
+
+class QuestionManager(models.Manager):
+
+  def new(self):
+    return self.order_by('-added_at')
+
+  def popular(self):
+    return self.order_by('-rating')
